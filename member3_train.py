6e84b246f719d60e7a957ecb5ba751194b52f86d
GIRIPOGU VIKAS JOEL <ad25b1010@iiitr.ac.in>
2026-06-08 22:18:07 +0530

    Update member3_train.py

--- member3_train.py
+++ member3_train.py
@@ -1,43 +1,43 @@
 from tensorflow.keras.optimizers import Adam
 
-from member1_dataset import NUM_CLASSES, get_datasets
+from member1_dataset import NUM_CLASSES, get_datasetss
 from member2_model import build_vgg16_model, my_loss
 
 
-EPOCHS = 10
+EPOCHS =   10
 MODEL_FILE = "fruit_model.keras"
 CLASS_NAMES_FILE = "class_names.txt"
 
 
 def save_class_names(class_names):
     
-    file = open(CLASS_NAMES_FILE, "w")
+    file = open(CLASS_NAMES_FILE,"w")
 
     for name in class_names:
-        file.write(name + "\n")
+        file.write(name+"\n")
 
     file.close()
 
-    print("Class names saved to", CLASS_NAMES_FILE)
+   print("Class names saved to", CLASS_NAMES_FILE)
 
 
 def train():
 
-    print("Loading dataset...")
+    print("Loading dataset")
     train_data, val_data, class_names = get_datasets()
 
-    print("Building model...")
+    print("Building model")
     model = build_vgg16_model(NUM_CLASSES)
 
     optimizer = Adam(learning_rate=0.0001)
 
     model.compile(
-        optimizer=optimizer,
-        loss=my_loss,
-        metrics=["accuracy"]
+        optimizer = optimizer,
+        loss = my_loss,
+        metrics = ["accuracy"]
     )
 
-    print("Training started... this may take a while")
+    print("Training started this may take a while")
     model.fit(
         train_data,
         validation_data=val_data,
@@ -48,7 +48,7 @@ def train():
     save_class_names(class_names)
 
     print("Training complete!")
-    print("Model saved as:", MODEL_FILE)
+    print("Model saved as:",MODEL_FILE)
 
 train()
 
