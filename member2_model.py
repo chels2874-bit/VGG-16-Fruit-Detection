3f88d1189f5996d574e10ce811e95b267a15d72c
Chels Banshiwal <chels2874@gmail.com>
2026-06-08 22:12:52 +0530

    Update member2_model.py

--- member2_model.py
+++ member2_model.py
@@ -1,23 +1,26 @@
 import tensorflow as tf
 from tensorflow.keras.applications import VGG16
-from tensorflow.keras.models import Sequentials 
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Dropout
 
 def my_loss(y_true, y_pred):
-
+  return tf.keras.losses.categorical_crossentropy(y_true, y_pred)
+  
+def build_vgg16_model(num_classes):
+  
   vgg_base = VGG16(
   weights= "imagenet",
-    includde_top=False,
-    input_shap= (224, 224, 3)
+    include_top=False,
+    input_shape=(224, 224, 3)
   )
 
-for ayer in vgg_base.layers:
+for layer in vgg_base.layers:
   layer.trainable = False
 
 model = Sequential()
 model.add(vgg_base)
 model.add(Flatten())
-model.add(Dense(128, activations="relu"))
+model.add(Dense(128, activation="relu"))
 model.add(Dropout(0.3))
 model.add(Dense(num_classes, activation="softmax")
 
